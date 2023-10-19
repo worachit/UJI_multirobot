8fcfcab4be16a02d0a5fa2dacae988ec80839a6d
worachit <worachit1@gmail.com>
2023-10-19 14:16:05 +0200

    add compile and run

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p build
+mkdir build
 
 # build exec for cpp
 
@@ -14,7 +14,3 @@ make -j
 # cd build
 # cmake ../ -DPYTHON=true
 # make -j
-
-./lifelong --inputFile ../example_problems/warehouse.domain/warehouse_small_50.json -o ../../PlanViz/example/test.json
-cd ../../PlanViz/script
-python3 plan_viz.py --map ../example/warehouse_small.map --plan ../example/test.json --grid --aid --static --ca
